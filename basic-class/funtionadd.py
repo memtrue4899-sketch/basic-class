20c5450357b2f719904120410d12e18260ab8724
Supanat5847 <supanat5847@gmail.com>
2026-09-06 15:21:48 +0700

    exponent

---
+++ basic-class/funtionadd.py
@@ -0,0 +1,5 @@
+from main import calc
+import pandas as pd
+if __name__ == "__main__":
+    x = calc(20, 20)
+    print(x.add())
---
+++ basic-class/main.py
@@ -0,0 +1,17 @@
+class calc:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y 
+    def add(self):
+        return self.x + self.y
+    def sub(self):
+        return self.x - self.y
+    def mul(self):
+        return self.x * self.y
+
+    def power(self):
+        return self.x ** self.y
+
+if __name__ == "__main__":
+    a = calc(30, 20)
+    print(a.add())
